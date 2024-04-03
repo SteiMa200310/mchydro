ff24f5214b9230535dea63a827baeeef62c2587b
MatthiasSteininger <matti.steininger@gamil.com>
2024-04-03 16:13:29 +0200

    HydroPluginV1 - init

---
+++ HydroPluginV1/src/main/java/com/mdfg/hydropluginv1/HydroPluginV.java
@@ -0,0 +1,17 @@
+package com.mdfg.hydropluginv1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HydroPluginV extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
