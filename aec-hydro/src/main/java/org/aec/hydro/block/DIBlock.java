177bd2e164e132f519fb1263ff3825d12684bbf0
Ehrenhuber Simon <40146720200163@litec.ac.at>
2024-04-22 15:10:21 +0200

    Update repo

---
+++ aec-hydro/src/main/java/org/aec/hydro/block/DIBlock.java
@@ -0,0 +1,157 @@
+package org.aec.hydro.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import org.aec.hydro.AECHydro;
+import org.aec.hydro.utils.Config;
+import org.aec.hydro.utils.ConfigHelper;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DIBlock extends Block {
+
+    // see PressurePlateBlock.class, AbstractPressurePlateBlock.class
+    public static final BooleanProperty POWERED = Properties.POWERED;
+
+    public DIBlock(Settings settings) {
+        super(settings);
+        this.setDefaultState(this.getDefaultState().with(Properties.POWERED, false));
+    }
+
+    // set / return custom name for block
+    @Override
+    public MutableText getName() {
+        return Text.translatable("block.hydro.di_block");
+    }
+
+    // if block is being broken, remove it from list
+    @Override
+    public void onBroken(WorldAccess world, BlockPos pos, BlockState state) {
+
+        // check if event is being called on server, because it is called on server and client, therefore triggered twice
+        // code can only be run on server because it accesses the config file
+        if (world.isClient()) return;
+
+        // check if block is in DI/DO list
+        JSONObject config = Config.getConfig();
+        JSONArray diBlockList = config.getJSONObject("di_do_list").getJSONArray("di");
+
+        // return if block is not in list
+        if (!ConfigHelper.posInList(diBlockList, pos)) return;
+
+        // remove block from list and update config
+        int indexInList = ConfigHelper.getIndexForPos(diBlockList, pos);
+        diBlockList.remove(indexInList);
+        Config.updateConfig(config);
+    }
+
+    // right-click event
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+
+        // check if event is being called on server, because it is called on server and client, therefore triggered twice
+        if (world.isClient()) return ActionResult.SUCCESS;
+
+        // check if block has already been added to DI/DO list
+        JSONObject config = Config.getConfig();
+        JSONArray diBlockList = config.getJSONObject("di_do_list").getJSONArray("di");
+        JSONArray doBlockList = config.getJSONObject("di_do_list").getJSONArray("do");
+        ArrayList<Object> usedDoPinList = new ArrayList<>();
+        List<Object> pinList = config.getJSONObject("pin_list").getJSONArray("pins").toList();
+
+        // remove pins used by DOs from available pins
+        for (int i = 0; i < doBlockList.length(); i += 1) {
+            usedDoPinList.add(doBlockList.getJSONObject(i).get("pin"));
+        }
+        // remove all pins used by DOs
+        pinList.removeAll(usedDoPinList);
+
+        //System.out.println(usedDoPinList);
+        //System.out.println(pinList);
+
+        // if block has not been added to list, add it to list with default configuration
+        if (!ConfigHelper.posInList(diBlockList, pos)) {
+
+            JSONObject object = new JSONObject();
+            object.put("pin", -1);
+            object.put("pos", ConfigHelper.blockPosToString(pos));
+
+            diBlockList.put(object);
+
+            //System.out.println(config);
+            Config.updateConfig(config);
+
+        }
+
+        // increment pin and display to player
+        int indexInList = ConfigHelper.getIndexForPos(diBlockList, pos);
+        int pin = (int) diBlockList.getJSONObject(indexInList).get("pin");
+
+        // cycle through list of pins
+        // increment pin to cycle through all possible pins
+        //System.out.println(pinList.indexOf(pin));
+
+        // if pin is not found in list, function returns -1 --> therefore set pin index to 0 (first)
+        // return next entry in pin list if pin is part of list, else use first entry
+        // at end of array, switch to first element to create a "cycle"
+        pin = (int) (pinList.contains(pin) ? ((pinList.indexOf(pin) < (pinList.size() - 1)) ? (pinList.get(pinList.indexOf(pin) + 1)) : (pinList.get(0))) : (pinList.get(0)));
+        //pin += 1;
+
+        // update pin in config file and print set pin to player's actionbar
+        diBlockList.getJSONObject(indexInList).put("pin", pin);
+        Config.updateConfig(config);
+
+        player.sendMessage(Text.of("Pin: " + pin), true);
+
+        return ActionResult.SUCCESS;
+    }
+
+    public int getRedstoneOutput(BlockState state) {
+        return (Boolean)state.get(POWERED) ? 15 : 0;
+    }
+
+    public void setRedstoneOutput(World world, BlockPos pos, boolean shouldPower) {
+        world.setBlockState(pos, world.getBlockState(pos).with(POWERED, shouldPower), 2);
+        updateNeighbors(world, pos);
+    }
+
+    protected int getRedstoneOutput(World world, BlockPos pos) {
+        return (Boolean)world.getBlockState(pos).get(POWERED) ? 15 : 0;
+    }
+
+    protected void updateNeighbors(World world, BlockPos pos) {
+        world.updateNeighborsAlways(pos, this);
+        world.updateNeighborsAlways(pos.down(), this);
+    }
+
+    public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+        return this.getRedstoneOutput(state);
+    }
+
+    // allows block to connect to redstone
+    @Override
+    public boolean emitsRedstonePower(BlockState state) {
+        return true;
+    }
+
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(POWERED);
+    }
+}
---
+++ aec-hydro/src/main/java/org/aec/hydro/utils/Config.java
@@ -0,0 +1,97 @@
+package org.aec.hydro.utils;
+
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.text.Text;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import static org.aec.hydro.AECHydro.LOGGER;
+
+import java.io.*;
+
+public class Config {
+
+    private static final String CONFIG_STRING = """
+            {
+                "_description": "Config file for Hydrogen in Minecraft, a mod for simulating the cycle of hydrogen",
+                
+                "di_do_list": {
+                    "_description": "list of blocks that act like DI/DOs",
+                    "di": [],
+                    "do": []
+                },
+                
+                "pin_list": {
+                    "_description": "list of pins that can be used (based on pi4j documentation, BCM pinout)",
+                    "pins": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27]
+                }
+            }
+            """;
+
+    public static void saveConfig() {
+        File fabDir = FabricLoader.getInstance().getConfigDir().toFile();
+        File dir = new File(fabDir, "hydro");
+
+        // if dir spsmc does not exist in config folder, create it
+        if (!dir.exists()) {
+            dir.mkdirs();
+
+            LOGGER.info(Text.translatable("generic.hydro.config_folder").getString());
+        }
+
+        // check if config exists, else save it to config.json
+        File f = new File(dir, "config.json");
+
+        if (!f.exists()) {
+
+            try {
+                f.createNewFile();
+                FileOutputStream outputStream = new FileOutputStream(f);
+
+                outputStream.write(CONFIG_STRING.getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            LOGGER.info(Text.translatable("generic.hydro.config_created").getString());
+        }
+
+    }
+
+    public static JSONObject getConfig() {
+
+        // stuff to get dir of config file
+        File fabDir = FabricLoader.getInstance().getConfigDir().toFile();
+        File dir = new File(fabDir, "hydro");
+        File f = new File(dir, "config.json");
+
+        // define reader
+        FileReader reader = null;
+
+        // Read the JSON file
+        try {
+            reader = new FileReader(f.getAbsolutePath());
+        } catch (FileNotFoundException e) {
+            LOGGER.error(String.valueOf(e));
+        }
+        // create AssertionError if file not found, will not crash game, just show up in console
+        assert reader != null;
+        JSONTokener tokener = new JSONTokener(reader);
+
+        return new JSONObject(tokener);
+    }
+
+    public static void updateConfig(JSONObject config) {
+
+        // stuff to get dir of config file
+        File fabDir = FabricLoader.getInstance().getConfigDir().toFile();
+        File dir = new File(fabDir, "hydro");
+        File f = new File(dir, "config.json");
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(f);
+            outputStream.write(config.toString(4).getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
