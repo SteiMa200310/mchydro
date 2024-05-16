aa86590cc75ef41d4df675ef86765ae511e78d4c
MatthiasSteininger <matti.steininger@gamil.com>
2024-05-16 15:07:32 +0200

    pipe edge testing

--- aec-hydro/src/main/java/org/aec/hydro/block/PipeV2.java
+++ aec-hydro/src/main/java/org/aec/hydro/block/PipeV2.java
@@ -2,6 +2,7 @@ package org.aec.hydro.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.entity.Hopper;
 import net.minecraft.item.ItemPlacementContext;
@@ -10,6 +11,9 @@ import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class PipeV2 extends HorizontalFacingBlock {
@@ -38,5 +42,20 @@ public class PipeV2 extends HorizontalFacingBlock {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(FACING);
+    }
+
+    @Override
+    public void onBlockAdded(BlockState state, World world, BlockPos pos, BlockState oldState, boolean notify) {
+        if (!world.isClient) {
+            Direction facing = state.get(FACING);
+            BlockPos neighbor1 = pos.offset(facing.rotateYClockwise());
+            BlockPos neighbor2 = pos.offset(facing.rotateYCounterclockwise());
+            BlockState neighborState1 = world.getBlockState(neighbor1);
+            BlockState neighborState2 = world.getBlockState(neighbor2);
+            if (neighborState1.getBlock() == this && neighborState2.getBlock() == this) {
+                // Replace this block with your desired block when placed between two other PipeV2 blocks
+                world.setBlockState(pos, Blocks.STONE.getDefaultState());
+            }
+        }
     }
 }
