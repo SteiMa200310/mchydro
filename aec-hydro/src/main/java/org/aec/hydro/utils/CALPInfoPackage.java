bac99bdde57725653348bedecc20fb6295e0d834
MatthiasSteininger <matti.steininger@gmail.com>
2024-09-01 21:42:48 +0200

    Git Merging / Pull Request Info Update
    //comment-update1
    //comment-update2
    //comment-update3
    
    //github by default merges using fast forward (ff)
    //but pull requests in general are using no-ff (no fast forward)
    
    //keep that in mind since it was a bit tricky to understand at first -> it is also worth noting and merge sqash / merge rebase
    //can sometimes be helpful too and its all about the usage case -> but to get the known github branch history that you also get when using prs
    //use no-ff
    
    //doing a pull request and then approving it yourself does pretty much the same thing so you can just skip that and immediatly do it like this
    //prs are just for repos YOU DO NOT OWN -> which is never the case in our scenario

--- aec-hydro/src/main/java/org/aec/hydro/utils/CALPInfoPackage.java
+++ aec-hydro/src/main/java/org/aec/hydro/utils/CALPInfoPackage.java
@@ -4,4 +4,14 @@ public class CALPInfoPackage {
     //comment-update1
     //comment-update2
     //comment-update3
+
+    //github by default merges using fast forward (ff)
+    //but pull requests in general are using no-ff (no fast forward)
+
+    //keep that in mind since it was a bit tricky to understand at first -> it is also worth noting and merge sqash / merge rebase
+    //can sometimes be helpful too and its all about the usage case -> but to get the known github branch history that you also get when using prs
+    //use no-ff
+
+    //doing a pull request and then approving it yourself does pretty much the same thing so you can just skip that and immediatly do it like this
+    //prs are just for repos YOU DO NOT OWN -> which is never the case in our scenario
 }
