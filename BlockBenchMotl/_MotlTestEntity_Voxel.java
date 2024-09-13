6ae3a266c5ba956b6cb8d4edb557a4f76a54af7f
EhreSi200163 <90833483+EhreSi200163@users.noreply.github.com>
2024-09-13 07:50:25 +0200

    Delete BlockBenchMotl directory

--- BlockBenchMotl/_MotlTestEntity_Voxel.java
+++
@@ -1,8 +0,0 @@
-public VoxelShape makeShape(){
-	VoxelShape shape = VoxelShapes.empty();
-	shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.12499999999999989, 0, 0.125, 0.8749999999999999, 0.125, 0.875));
-	shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.25, 0.125, 0.25, 0.75, 0.5, 0.75));
-	shape = VoxelShapes.union(shape, VoxelShapes.cuboid(0.12499999999999989, 0.5, 0.125, 0.8749999999999999, 0.625, 0.875));
-
-	return shape;
-}
