93d72a373e5a505d5b3b058f7ef4ad5e537fe17a
Ehrenhuber Simon <40146720200163@litec.ac.at>
2024-10-02 14:05:20 +0200

    Update: Adding Compare Powerlevel (new Error can't detect sucessfull execution in mc)

--- aec-hydro/src/main/java/org/aec/hydro/commands/base/CommandSetup.java
+++ aec-hydro/src/main/java/org/aec/hydro/commands/base/CommandSetup.java
@@ -1,6 +1,7 @@
 package org.aec.hydro.commands.base;
 
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import org.aec.hydro.commands.ComparePowerlevelCommand;
 import org.aec.hydro.commands.GetPowerlevelCommand;
 import org.aec.hydro.commands.SetPowerlevelCommand;
 import org.aec.hydro.commands.TogglePipeLookingDirectionCommand;
@@ -14,15 +15,13 @@ public class CommandSetup {
     public static List<ACommand> commandList = List.of(
         new TogglePipeLookingDirectionCommand("togglePipeLookingDirection"),
             new GetPowerlevelCommand("getPowerlevel"),
-            new SetPowerlevelCommand("setPowerlevel")
+            new SetPowerlevelCommand("setPowerlevel"),
+            new ComparePowerlevelCommand("comparePower")
     );
 
     public static void RegisterCommands() {
         commandList.forEach((command) ->
-            CommandRegistrationCallback.EVENT
-                .register(
-                        command::register
-                )
+            CommandRegistrationCallback.EVENT.register(command::register)
         );
     }
 }
