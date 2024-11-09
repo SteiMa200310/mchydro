4dc698b3fb8f167608921a5bbb2aa8a92cab3a98
Ehrenhuber Simon <s.ehrenhuber@outlook.de>
2024-11-09 17:34:12 +0100

    Update: Adding Permission for reset command

--- aec-hydro/src/main/java/org/aec/hydro/commands/ResetLevelCommand.java
+++ aec-hydro/src/main/java/org/aec/hydro/commands/ResetLevelCommand.java
@@ -5,6 +5,7 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.argument.BlockPosArgumentType;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ServerWorld;
@@ -24,17 +25,18 @@ public class ResetLevelCommand extends ACommand {
     @Override
     public void register(CommandDispatcher<ServerCommandSource> dispatcher, CommandRegistryAccess registryAccess, CommandManager.RegistrationEnvironment environment) {
         dispatcher.register(CommandManager.literal(Name)
-                .requires(source -> source.hasPermissionLevel(1))
+                .requires(source -> source.hasPermissionLevel(0))
                 .executes(this::Execute));
     }
 
     @Override
     public int Execute(CommandContext<ServerCommandSource> ctx) {
-        ServerCommandSource source = ctx.getSource();
+        MinecraftServer server = ctx.getSource().getServer();
+        server.execute(()-> {
+           server.getCommandManager().executeWithPrefix(server.getCommandSource().withLevel(4),"/function debug:reset_game");
+        });
 
-        source.getServer().getCommandManager().executeWithPrefix(source,"/function debug:reset_game");
-
-        source.sendFeedback(()-> Text.literal("Game reset"), true);
+        ctx.getSource().sendFeedback(()-> Text.literal("Game reset"), true);
         return 1;
     }
 }
